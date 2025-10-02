515731bd13b3b7ebd054d63b9da96993f4bcb1ef
Lucas Freitas <lucas1506freitas@gmail.com>
2025-10-02 02:51:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-478205522-c1aca",
+  "appId": "1:265784093223:web:89a27c0847f765817efc02",
+  "apiKey": "AIzaSyD4NtdweK2fiOYNB1HJdXKWwbPyfrZ_0sA",
+  "authDomain": "studio-478205522-c1aca.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "265784093223"
+};
